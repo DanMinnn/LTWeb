fdd74773ae435ffa764d1e442ceaee5825d85a92
DANG MINH <minhdangngoc304@gmail.com>
2023-10-14 16:23:07 +0700

    Tạo nút xóa, chỉnh sửa cập nhật

--- TestTemplate/Areas/Admin/Controllers/CoSoController.cs
+++ TestTemplate/Areas/Admin/Controllers/CoSoController.cs
@@ -26,7 +26,15 @@ namespace TestTemplate.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult ThemMoi(coso model, HttpPostedFileBase fileAnh)
         {
-            if(string.IsNullOrEmpty(model.macs) == true || string.IsNullOrEmpty(model.tencs) == true ||
+            if (fileAnh.ContentLength > 0)
+            {
+                string rootFolder = Server.MapPath("/Content/img/");
+                string pathImage = rootFolder + fileAnh.FileName;
+                fileAnh.SaveAs(pathImage);
+                model.hinhanh = fileAnh.FileName;
+            }
+
+            if (string.IsNullOrEmpty(model.macs) == true || string.IsNullOrEmpty(model.tencs) == true ||
                 string.IsNullOrEmpty(model.hinhanh) == true || string.IsNullOrEmpty(model.link) == true ||
                     string.IsNullOrEmpty(model.mucgia) == true || string.IsNullOrEmpty(model.diachi) == true)
             {
@@ -34,13 +42,7 @@ namespace TestTemplate.Areas.Admin.Controllers
                 return View(model);
             }
 
-            if (fileAnh.ContentLength > 0)
-            {
-                string rootFolder = Server.MapPath("/Content/img/");
-                string pathImage = rootFolder + fileAnh.FileName;
-                fileAnh.SaveAs(pathImage);
-            }
-
+           
             try
             {
                 db.cosoes.Add(model);
@@ -54,16 +56,24 @@ namespace TestTemplate.Areas.Admin.Controllers
             }
         }
 
-        public ActionResult CapNhat(string maCS)
+        public ActionResult CapNhat(string id)
         {
-            var model_Edit = db.cosoes.Find(maCS);
+            var model_Edit = db.cosoes.Find(id);
             return View(model_Edit);
         }
 
         [HttpPost]
         public ActionResult CapNhat(coso model_Edit, HttpPostedFileBase fileAnh)
         {
-            if (string.IsNullOrEmpty(model_Edit.macs) == true || string.IsNullOrEmpty(model_Edit.tencs) == true ||
+            if (fileAnh.ContentLength > 0)
+            {
+                string rootFolder = Server.MapPath("/Content/img/");
+                string pathImage = rootFolder + fileAnh.FileName;
+                fileAnh.SaveAs(pathImage);
+                model_Edit.hinhanh = fileAnh.FileName;
+            }
+
+            if (string.IsNullOrEmpty(model_Edit.tencs) == true ||
                 string.IsNullOrEmpty(model_Edit.hinhanh) == true || string.IsNullOrEmpty(model_Edit.link) == true ||
                     string.IsNullOrEmpty(model_Edit.mucgia) == true || string.IsNullOrEmpty(model_Edit.diachi) == true)
             {
@@ -71,12 +81,7 @@ namespace TestTemplate.Areas.Admin.Controllers
                 return View(model_Edit);
             }
             
-            if (fileAnh.ContentLength > 0)
-            {
-                string rootFolder = Server.MapPath("/Content/img/");
-                string pathImage = rootFolder + fileAnh.FileName;
-                fileAnh.SaveAs(pathImage);
-            }
+           
 
             var updateCoSo = db.cosoes.Find(model_Edit.macs);
             try
@@ -86,6 +91,7 @@ namespace TestTemplate.Areas.Admin.Controllers
                 updateCoSo.diachi = model_Edit.diachi;
                 updateCoSo.link = model_Edit.link;
                 updateCoSo.mucgia = model_Edit.mucgia;
+                updateCoSo.mals = model_Edit.mals;
 
                 db.SaveChanges();
                 return RedirectToAction("DanhSachCoSo");
@@ -95,6 +101,14 @@ namespace TestTemplate.Areas.Admin.Controllers
                 ModelState.AddModelError("", ex.Message);
                 return View(model_Edit);
             }
+        }
+
+        public ActionResult Xoa(string id)
+        {
+            var model = db.cosoes.Find(id);
+            db.cosoes.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("DanhSachCoSo");
         }
     }
 }
