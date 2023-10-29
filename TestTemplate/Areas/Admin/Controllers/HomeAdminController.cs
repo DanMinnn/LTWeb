11039afe2d4a308ef1c8117d1692b9dca276b29e
DANG MINH <minhdangngoc304@gmail.com>
2023-10-29 15:03:19 +0700

    Admin login

--- TestTemplate/Areas/Admin/Controllers/HomeAdminController.cs
+++ TestTemplate/Areas/Admin/Controllers/HomeAdminController.cs
@@ -14,42 +14,39 @@ namespace TestTemplate.Areas.Admin.Controllers
         QLDSEntities db = new QLDSEntities();
         public ActionResult Index()
         {
-            if(Session["user"] == null)
-                return RedirectToAction("DangNhap");
-            else 
                 return View();
         }
 
-        public ActionResult DangNhap()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult DangNhap(string user, string password)
-        {
-            //Check db
+        //public ActionResult DangNhap()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult DangNhap(string user, string password)
+        //{
+        //    //Check db
                
-            //Check code
-            if(user.ToLower() == "admin" && password == "1234")
-            {
-                Session["user"] = "admin"; // phiên làm việc;
-                return RedirectToAction("Index");
-            }    
-            else
-            {
-                TempData["error"] = "Tên đăng nhập hoặc mật khẩu không đúng !";
-                return View();
-            }
-        }
+        //    //Check code
+        //    if(user.ToLower() == "admin" && password == "1234")
+        //    {
+        //        Session["user"] = "admin"; // phiên làm việc;
+        //        return RedirectToAction("Index");
+        //    }    
+        //    else
+        //    {
+        //        TempData["error"] = "Tên đăng nhập hoặc mật khẩu không đúng !";
+        //        return View();
+        //    }
+        //}
 
         public ActionResult DangXuat()
         {
             //xóa session
-            Session.Remove("user");
+            Session.Remove("admin");
             // Xóa authentication form
             FormsAuthentication.SignOut();
 
-            return RedirectToAction("DangNhap");
+            return RedirectToAction("DangNhap", "DangNhap", new { area = "" });
         }
 
         //public ActionResult DanhSachCoSo()
