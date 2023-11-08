a888c175de2aa2eae73c818418bf01a39efaac5d
DANG MINH <minhdangngoc304@gmail.com>
2023-11-08 10:43:24 +0700

    TimKiem in Admin

---
+++ TestTemplate/Models/TimModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestTemplate.Models;
+
+namespace TestTemplate.Models
+{
+    public class TimModel
+    {
+        public List<CoSo> CoSo { get; set; }
+        public List<NhanVien> NhanVien { get; set; }
+        public List<user_KhachHang> KhachHang { get; set; }
+        public string sTuKhoa { get; set; }
+    }
+}
